51028765d257f46a42dbad1ddb3d57e07df6e620
Banerjee Aditya <aditya.banerjee@activate.sg>
2023-11-10 18:30:50 +0800

    add env

--- frontend/src/utils/routing.ts
+++ frontend/src/utils/routing.ts
@@ -2,7 +2,7 @@ const { NODE_ENV } = process.env;
 
 export const getRoute = (path: string) => {
     if(path === "/") {
-        return NODE_ENV === "development" ? 'index' : 'index.html';
+        return NODE_ENV === "development" ? '/index' : '/index.html';
     }
     return NODE_ENV === "development" ? path : `${path}.html`;
 };
